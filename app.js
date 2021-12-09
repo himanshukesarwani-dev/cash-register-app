c98d1e1881ad6baf5e8d73f015de164fecbf381a
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2021-12-09 22:47:13 +0530

    chore: added 3 js var

---
+++ app.js
@@ -0,0 +1,8 @@
+const billAmount = document.querySelector("#bill-amount");
+const cashGiven = document.querySelector("#cash-given");
+const checkButton = document.querySelector("#check-btn");
+
+
+
+
+
