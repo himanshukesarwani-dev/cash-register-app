8fea768285aee9271ea03643d4b53bbdf2e7fcc7
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2022-01-03 23:54:45 +0530

    chore: added bill amount validator function

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-btn");
+const message = document.querySelector("#error-message")
 
+function validateBillAndCashAmount() {
 
+}
 
+checkButton.addEventListener("click", function validateBillAndCashAmount() {
+    message.style.display = "none";
+    if (billAmount.value > 0) {
+        console.log("bill amount is greater than zero")
+    } else {
+        message.style.display = "block"
+        message.innerText = "The bill amount should be greater than zero";
+    }
 
-
+});
