9e1c02a74437d6b9140e2ae6f85193a4bdf82882
Himanshu Kesarwani <himanshukesarwani800@gmail.com>
2022-01-05 00:52:32 +0530

    chore: added calculate change func

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const billAmount = document.querySelector("#bill-amount");
 const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-btn");
-const message = document.querySelector("#error-message")
+const message = document.querySelector("#error-message");
+const NotesAvailable = [2000, 500, 200, 100, 50, 20, 10, 5, 1];
+const noOfNotes = document.querySelectorAll(".no-of-notes")
 
 function validateBillAndCashAmount() {
 
@@ -10,22 +12,51 @@ function validateBillAndCashAmount() {
 checkButton.addEventListener("click", function validateBillAndCashAmount() {
     hideMessage();
     if (billAmount.value > 0) {
-     
-        console.log("bill amount is greater than zero");
-    } 
-    
-    else {
-       showMessage("Bill Amount be greater than zero. Invalid bill amount");
+
+
+        if (billAmount.value > cashGiven.value) {
+            showMessage("Pay more as your bill amount is greater than the cash you gave.")
+        } else if (billAmount.value == cashGiven.value) {
+            showMessage("Bill amount is exactly equal to cash given. Hence no change needed to be returned")
+        } else {
+            const returnAmount = cashGiven.value - billAmount.value;
+            calculateChange(returnAmount);
+        }
+
+    } else {
+        showMessage("Bill Amount be greater than zero. Invalid bill amount");
     }
 
 });
 
 
-function hideMessage(){
+function hideMessage() {
     message.style.display = "none";
 }
 
-function showMessage(msg){
+function showMessage(msg) {
     message.style.display = "block";
     message.innerText = msg;
 }
+
+function calculateChange(rtnAmt) {
+    for (let i = 0; i < NotesAvailable.length; i++) {
+    const totalNotes = Math.trunc(rtnAmt/NotesAvailable[i]);
+    rtnAmt %= NotesAvailable[i];
+noOfNotes[i].innerHTML = totalNotes;
+    }
+}
+
+
+//100
+/* 2000
+
+
+1900
+
+
+1900//2000
+1900%2000
+
+
+*/
